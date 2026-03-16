4c6b2e1c7b434d693b3c2d6ae12b3324a26e96f2
Leonel Garrido <legarrido@unicesar.edu.co>
2026-03-15 21:46:10 -0500

    feature: "implementar clase Ticket y calculo de tarifa"

---
+++ src/model/Ticket.java
@@ -0,0 +1,34 @@
+package model;
+
+import interfaces.Calculable;
+
+public class Ticket implements Calculable {
+
+    private Pasajero pasajero;
+    private String placaVehiculo;
+    private String origen;
+    private String destino;
+    private double tarifaBase;
+
+    public Ticket(Pasajero pasajero, String placaVehiculo, String origen, String destino, double tarifaBase) {
+        this.pasajero = pasajero;
+        this.placaVehiculo = placaVehiculo;
+        this.origen = origen;
+        this.destino = destino;
+        this.tarifaBase = tarifaBase;
+    }
+
+    @Override
+    public double calcularTotal() {
+        double descuento = pasajero.calcularDescuento();
+        return tarifaBase - (tarifaBase * descuento);
+    }
+
+    public String toFile() {
+        return pasajero.getCedula() + ";" +
+                placaVehiculo + ";" +
+                origen + ";" +
+                destino + ";" +
+                calcularTotal();
+    }
+}
